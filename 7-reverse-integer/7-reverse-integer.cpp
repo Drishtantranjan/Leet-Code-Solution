97311e21331f9aef8b786448d3911199d9b6dcd2
Drishtant Ranjan <84273332+Drishtantranjan@users.noreply.github.com>
2022-04-04 21:34:23 +0530

    Time: 0 ms (100.00%), Space: 6 MB (32.49%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -5,8 +5,9 @@ public:
      while(x!=0)
         {
        int dig = x%10;
-         if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && dig > 7)) return 0;
-            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && dig < -8)) return 0;
+         if((rev>INT_MAX/10)||(rev<INT_MIN/10)){
+             return 0;
+         }
          rev = rev*10 + dig;
              x=x/10; 
                   }
